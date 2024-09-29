41cb5816549792090aff51cf8b8297f86e750bb3
HP <HP@DESKTOP-7MPQEPN>
2024-09-29 20:07:04 +0530

    first comit

---
+++ src/iplteams/Test.java
@@ -0,0 +1,38 @@
+package iplteams;
+
+public class Test {
+public static void main(String[] args) {
+		
+		
+		IPLteams t = new IPLteams();
+		
+		
+		System.out.println("List of all players(batters)");
+		t.batterAlL();
+		
+		System.out.println("List Of all Batter in RCB :");
+		t.batterrcb();
+		
+		System.out.println("List Of all Batter in CSK :");
+		t.battercsk();
+		
+		System.out.println("List Of all Batter in MI :");
+		t.battermi();
+		
+		
+		System.out.println("Bowlers with more than 50 wickets:");
+		t.bowlerAll();
+		
+		System.out.println("List of all bowler in RCB : ");
+		t.bowlerrcb();
+		
+		
+		System.out.println("List Of All Allrounnders in IPL : ");
+		t.allRoundersAll();
+		
+		t.startsWith();
+	}
+	
+	
+}
+
